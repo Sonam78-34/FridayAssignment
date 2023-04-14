6fbafa70d33d5a28dface6c499877c09fc2a27d7
Sonam Kumari <126173613+Sonam78-34@users.noreply.github.com>
2023-04-14 20:20:45 +0530

    Add files via upload
    First Test Assignment(Library Management System)

---
+++ LibraryManagementSystem.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem
+{
+    public class LibraryManagementSystem
+    {
+       //for borrower information of login
+
+        public void BorrowerInformation(int numberOfBorrower,string[] borrowerName)
+        {
+            
+            Dictionary<int, string> borrowerList = new Dictionary<int, string>();
+           
+            for (int i = 0; i <numberOfBorrower; i++)
+            {
+                if (numberOfBorrower > 1)
+                {
+                    
+                    borrowerList.Add(i+1, borrowerName[i]);
+                }
+
+            }
+            Console.WriteLine("The borrowers are : ");
+            foreach (KeyValuePair<int, string> keyValue in borrowerList)
+            {
+                Console.WriteLine("{0}  {1}",keyValue.Key,keyValue.Value);
+            }
+        }
+
+
+        public void IssuerInformation(string issuerName, string issuerCode)
+        {
+            Console.WriteLine("The issuer name is {0} and the code of issuer is {1}", issuerName, issuerCode);
+
+
+        }
+
+
+
+        //Books information
+        public void BooksAvailableInLibrary(Dictionary<string,int> books, string borrowerBookName)
+        {
+            Console.WriteLine(" ");
+            Console.WriteLine("The books available in the library are :");
+            foreach(KeyValuePair<string, int> book in books)
+            {
+                Console.WriteLine(book);
+            }
+            Console.WriteLine(" ");
+
+            
+            foreach(var book in books.Keys)
+            {
+                
+                if(book==borrowerBookName)
+                {
+                    Console.WriteLine("The book is availabe borrower can borrow the book");
+                }
+
+
+            }
+
+            foreach (var count in books.Values)
+            {
+                
+                if (count == 0 && books.ContainsKey(borrowerBookName)) 
+                {
+                    Console.WriteLine("The book is already taken");
+                    
+                }
+            }
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System.Threading.Channels;
+namespace LibraryManagementSystem
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            LibraryManagementSystem libraryManagementSystem = new LibraryManagementSystem();
+            Console.WriteLine("Enter the number of borrower");
+            int numberOfBorrower= int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the borrower name");
+            string[] borrowerName= new string[numberOfBorrower];
+            for(int i = 0; i < numberOfBorrower; i++)
+            {
+                borrowerName[i] = Console.ReadLine();
+            }
+
+            libraryManagementSystem.BorrowerInformation(numberOfBorrower,borrowerName);
+
+
+            //for issuer information
+            Console.WriteLine("Enter the issuer name");
+            string issuerName = Console.ReadLine();
+            Console.WriteLine("Enter the issuer code");
+            string issuerCode = Console.ReadLine();
+            libraryManagementSystem.IssuerInformation(issuerName, issuerCode);
+
+
+
+            //for making the taken or not
+
+
+
+
+
+
+
+            //creating book collection in library
+            Dictionary<string,int> books = new Dictionary<string, int> ();
+            books.Add("BhagawatGita", 2);
+            books.Add("Ramayan",1);
+            books.Add("atomic habit", 0);
+
+
+            Console.WriteLine("Enter the book name which borrower want");
+            string borrowerBookName = Console.ReadLine();
+
+
+            libraryManagementSystem.BooksAvailableInLibrary(books,borrowerBookName);
+
+
+
+           
+
+
+
+            // information about book issued already or not available
+
+            //Console.WriteLine("Enter the book name which borrower want");
+            //string borrowerBookName= Console.ReadLine();
+
+            //libraryManagementSystem.displayBookIssuedOrNotAvailable(borrowerBookName);
+
+
+        }
+    }
+}
